b1e17280c73f065f7703f9f25e4695f04d2cfd13
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2021-01-14 13:20:14 -0300

    ajuste

--- demo/index.js
+++ demo/index.js
@@ -1,32 +1,29 @@
 // import CallingExtensions, { Constants } from "@hubspot/calling-extensions-sdk";
 
-import CallingExtensions from "./CallingExtensions";
-import {
-  errorType
-} from "./Constants";
+import CallingExtensions from './CallingExtensions';
+import { errorType } from './Constants';
 
-
-var engagementIdconst = "";
+var engagementIdconst = '';
 
 var Thread = {
-  sleep: function(ms) {
+  sleep: function (ms) {
     var start = Date.now();
 
     while (true) {
-      var clock = (Date.now() - start);
+      var clock = Date.now() - start;
       if (clock >= ms) break;
     }
-
-  }
+  },
 };
 
 const callback = () => {
-  var callId = "";
-  var hubId = "";
-  var called = "";
+  var callId = '';
+  var hubId = '';
+  var emailUser = '';
+  var called = '';
 
   let rowId = 0;
-  const incomingMsgContainer = document.querySelector("#incomingMsgs");
+  const incomingMsgContainer = document.querySelector('#incomingMsgs');
 
   function appendMsg(data, event) {
     // const div = document.createElement("div");
@@ -39,7 +36,7 @@ const callback = () => {
 
   const defaultSize = {
     width: 400,
-    height: 600
+    height: 600,
   };
 
   const state = {};
@@ -50,29 +47,25 @@ const callback = () => {
       onReady: () => {
         cti.initialized({
           isLoggedIn: true,
-          sizeInfo: defaultSize
+          sizeInfo: defaultSize,
         });
       },
       onDialNumber: (data, rawEvent) => {
         // appendMsg(data, rawEvent);
-        console.log("aqui!");
-        const {
-          phoneNumber
-        } = data;
+        console.log('aqui!');
+        const { phoneNumber } = data;
         state.phoneNumber = phoneNumber;
         window.setTimeout(
           () =>
-          cti.outgoingCall({
-            createEngagement: true,
-            phoneNumber
-          }),
-          500
+            cti.outgoingCall({
+              createEngagement: true,
+              phoneNumber,
+            }),
+          500,
         );
       },
       onEngagementCreated: (data, rawEvent) => {
-        const {
-          engagementId
-        } = data;
+        const { engagementId } = data;
         state.engagementId = engagementId;
         engagementIdconst = state.engagementId;
         // appendMsg(data, rawEvent);
@@ -85,143 +78,183 @@ const callback = () => {
       },
       onVisibilityChanged: (data, rawEvent) => {
         // appendMsg(data, rawEvent);
-      }
-    }
+      },
+    },
   });
 
-  const element = document.querySelector(".controls");
+  const element = document.querySelector('.controls');
 
-  element.addEventListener("click", event => {
+  element.addEventListener('click', (event) => {
     const clickedButtonValue = event.target.value;
     switch (clickedButtonValue) {
-      case "initialized":
+      case 'initialized':
         cti.initialized({
-          isLoggedIn: true
+          isLoggedIn: true,
         });
         break;
-      case "log in":
+      case 'log in':
         cti.userLoggedIn();
         break;
-      case "log out":
+      case 'log out':
         cti.userLoggedOut();
         break;
-        // Calls
-      case "incoming call":
+      // Calls
+      case 'incoming call':
         window.setTimeout(() => {
           cti.incomingCall();
         }, 500);
         break;
-      case "INICIAR CHAMADA":
+      case 'INICIAR CHAMADA':
         window.setTimeout(() => {
           cti.outgoingCall({
-            createEngagement: "true",
-            phoneNumber: state.phoneNumber
+            createEngagement: 'true',
+            phoneNumber: state.phoneNumber,
           });
-          hubId = localStorage.getItem("hubId");
+          hubId = localStorage.getItem('hubId');
+          emailUser = localStorage.getItem('email');
           var call = {
-            caller: "1000",
-            called: state.phoneNumber
-          }
+            caller: '1000',
+            called: state.phoneNumber,
+          };
           $.ajax({
-            url: "https://hubapi.falemaisvoip.com.br:8443/fale-mais/v1/calls/?hubId=" + hubId,
-            type: "post",
+            url:
+              'https://hubapi.falemaisvoip.com.br:8443/fale-mais/v1/calls/?hubId=' +
+              hubId +
+              '&email=' +
+              emailUser,
+            type: 'post',
             data: JSON.stringify(call),
             headers: {
-              "Content-Type": "application/json"
+              'Content-Type': 'application/json',
             },
-            success: function(data) {
+            success: function (data) {
               callId = data.callId;
               // console.log(validacaoChamada(callId));
               validacaoChamada(callId);
               function validacaoChamada(callId) {
-                    $.ajax({
-                      url: "https://hubapi.falemaisvoip.com.br:8443/fale-mais/v1/calls?callId=" + callId + "&hubId=" + hubId,
-                      type: "get",
-                      data: "",
-                      headers: {
-                        "Content-Type": "application/json"
-                      },
-                      success: function(dataStatus) {
-                        console.log(dataStatus);
-                        if(dataStatus.state === "finished" || dataStatus.state === "failed" || dataStatus.state === "string" || dataStatus === "" || dataStatus == null || dataStatus == undefined){
-                          $.ajax({
-                            url: "https://hubapi.falemaisvoip.com.br:8443/hubspot/v1/api/contact/engagement?callId=" + callId + "&called=" + state.phoneNumber + "&engagementId=" + state.engagementId + "&hubId=" + hubId + "&end=1",
-                            type: "post",
-                            data: "",
-                            headers: {
-                              "Content-Type": "application/json"
-                            },
-                            success: function(dataEndCall) {
-                              console.log(dataEndCall);
-                            },
-                            error: function(error) {
-                              result = error;
-                              console.log(error);
-                              // alert("Erro no processo");
-                            }
-                          });
-                          cti.callEnded();
-                        }else{
-                            setTimeout(validacaoChamada(callId), 100000);
-                        }
-                      },
-                      error: function(errorDataStatus) {
-                        console.log(errorDataStatus);
-                        // alert("Erro no processo");
-                      }
-                    });
-                }
+                $.ajax({
+                  url:
+                    'https://hubapi.falemaisvoip.com.br:8443/fale-mais/v1/calls?callId=' +
+                    callId +
+                    '&hubId=' +
+                    hubId +
+                    '&email=' +
+                    emailUser,
+                  type: 'get',
+                  data: '',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  success: function (dataStatus) {
+                    console.log(dataStatus);
+                    if (
+                      dataStatus.state === 'finished' ||
+                      dataStatus.state === 'failed' ||
+                      dataStatus.state === 'string' ||
+                      dataStatus === '' ||
+                      dataStatus == null ||
+                      dataStatus == undefined
+                    ) {
+                      $.ajax({
+                        url:
+                          'https://hubapi.falemaisvoip.com.br:8443/hubspot/v1/api/contact/engagement?callId=' +
+                          callId +
+                          '&called=' +
+                          state.phoneNumber +
+                          '&engagementId=' +
+                          state.engagementId +
+                          '&hubId=' +
+                          hubId +
+                          '&end=1' +
+                          '&email=' +
+                          emailUser,
+                        type: 'post',
+                        data: '',
+                        headers: {
+                          'Content-Type': 'application/json',
+                        },
+                        success: function (dataEndCall) {
+                          console.log(dataEndCall);
+                        },
+                        error: function (error) {
+                          result = error;
+                          console.log(error);
+                          // alert("Erro no processo");
+                        },
+                      });
+                      cti.callEnded();
+                    } else {
+                      setTimeout(validacaoChamada(callId), 100000);
+                    }
+                  },
+                  error: function (errorDataStatus) {
+                    console.log(errorDataStatus);
+                    // alert("Erro no processo");
+                  },
+                });
+              }
             },
-            error: function(error) {
+            error: function (error) {
               console.log(error);
               // alert("Erro no processo");
-            }
+            },
           });
         }, 500);
         break;
-      case "call answered":
+      case 'call answered':
         cti.callAnswered();
         break;
-      case "FINALIZAR CHAMADA":
+      case 'FINALIZAR CHAMADA':
         var endCall = {
           callId: callId,
-          called: state.phoneNumber
-        }
+          called: state.phoneNumber,
+        };
         $.ajax({
-          url: "https://hubapi.falemaisvoip.com.br:8443/hubspot/v1/api/contact/engagement?callId=" + callId + "&called=" + state.phoneNumber + "&engagementId=" + state.engagementId + "&hubId=" + hubId + "&end=0",
-          type: "post",
-          data: "",
+          url:
+            'https://hubapi.falemaisvoip.com.br:8443/hubspot/v1/api/contact/engagement?callId=' +
+            callId +
+            '&called=' +
+            state.phoneNumber +
+            '&engagementId=' +
+            state.engagementId +
+            '&hubId=' +
+            hubId +
+            '&end=0' +
+            '&email=' +
+            emailUser,
+          type: 'post',
+          data: '',
           headers: {
-            "Content-Type": "application/json"
+            'Content-Type': 'application/json',
           },
-          success: function(dataEndCall) {
+          success: function (dataEndCall) {
             console.log(dataEndCall);
           },
-          error: function(error) {
+          error: function (error) {
             result = error;
             console.log(error);
             // alert("Erro no processo");
-          }
+          },
         });
         cti.callEnded();
         break;
-      case "call completed":
+      case 'call completed':
         cti.callCompleted({
-          engagementId: state.engagementId
+          engagementId: state.engagementId,
         });
         break;
-      case "send error":
+      case 'send error':
         cti.sendError({
           type: errorType.GENERIC,
-          message: "This is a message shown in Hubspot UI"
+          message: 'This is a message shown in Hubspot UI',
         });
         break;
-      case "change size":
+      case 'change size':
         defaultSize.width += 20;
         defaultSize.height += 20;
         cti.resizeWidget({
           width: defaultSize.width,
-          height: defaultSize.height
+          height: defaultSize.height,
         });
         break;
       default:
@@ -230,13 +263,12 @@ const callback = () => {
   });
 };
 
-
 if (
-  document.readyState === "interactive" ||
-  document.readyState === "complete"
+  document.readyState === 'interactive' ||
+  document.readyState === 'complete'
 ) {
   console.log(window);
   window.setTimeout(() => callback(), 1000);
 } else {
-  document.addEventListener("DOMContentLoaded", callback);
+  document.addEventListener('DOMContentLoaded', callback);
 }
